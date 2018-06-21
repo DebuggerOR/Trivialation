3249a4ccac1d17a79f5392054131e7da733badc3
roiDaniela <roi.fogler@gmail.com>
2018-06-21 18:32:17 +0300

    send player back to login client

--- public/main.js
+++ public/main.js
@@ -3,44 +3,12 @@
 
 let player = {username: 'Yocheved', password: "fkdxlxjgkj"};
 
+// Wait to PostsRepository do Async call
+let game = new Game(player);
+game.registerGameEvents();
+
 $( document ).ready(function() {
     $("#includedContent").load("./login/login.html", function(){
         let login = new Login();
     });
 });
-
-
-let  loadConfigureGameHandlebar = function () {
-    $.ajax({
-        method: "POST",
-        url: 'https://opentdb.com/api_category.php',
-        dataType: "json",
-        success: (data) =>{
-            console.log(Handlebars);
-            var theCompiledHtml = Handlebars.templates.set_game(data);
-            $('.configure-game-container').append(theCompiledHtml);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(textStatus);
-        }
-    });
-
-}
-
-
-/* eventsHandler.registerAddPost();
-eventsHandler.registerRemovePost();
-eventsHandler.registerToggleEditPost();
-eventsHandler.registerEditPost();
-eventsHandler.registerToggleComments();
-eventsHandler.registerAddComment();
-eventsHandler.registerRemoveComment();
-
-
-//-----Load  all the posts from database when page is loaded--------//
-
-
-
-postsRepository.loadPostsFromDB()
-.then(() => {postsRenderer.renderPosts(postsRepository.posts)})
-.catch(() => {console.log("error in rendering posts")}); */
