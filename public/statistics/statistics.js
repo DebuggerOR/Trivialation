862d509b7936de9cffd1c601f81468b88a69ca67
unknown <carlwanunu@gmail.com>
2018-06-24 02:16:00 +0300

    v4

--- public/statistics/statistics.js
+++ public/statistics/statistics.js
@@ -6,47 +6,25 @@
 
 
 
-
-
-     getScoreArray() {
-         //score_array_by_cat = 
-         db.stat.find({player:"davis", category: "football"}, 
-            {score: 1} );~
-     }
-
-     getSpeedArray() {
-        //speed_array_by_cat = 
-        db.stat.find({player:"davis", category: "football"}, 
-           {avg_time: 1} );
-    }
-
-
     
-
-
-     globalScore() {
-
-     }
-
-
-
-
-     record() {
-         max
-     }
-
-
-
-
-
-
+     getMaxScore(){
+        return $.ajax({
+             method: "GET",
+             url: '/stat-record',
+             success:(postsResult) => {
+                 console.log(postsResult);
+                 for(var post of postsResult){  
+                    this.posts.push({_id:post._id, text: post.text, comments: post.comments});
+                 } 
+             },
+             error: function (jqXHR, textStatus, errorThrown) {
+                 console.log(textStatus);
+             }
+         });
+        }
 
 
 
-
-
-
-
-
+        
 
  }
--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// var http = require('http');
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/MYDB";
+
 /**
  * Connect to port 8080.
  */
@@ -52,5 +56,37 @@ app.post('/login', function(request, response) {
         response.send("login success");
     }
 });
+
+
+
+app.get('/stat-record', function(request, response) {
+   Game.findOne({ player: '5b2bdf855876fb3f002428ed' })
+  .sort('-score')  // give me the max
+  .exec(function (error, result) {
+    if(error) { return console.error(error); }
+            response.send(result.score);
+  });
+}); 
+
+
+
+// app.get('/stat-record', function(request, db) {
+//     db.games.aggregate([{$group:{_id: "$player", record:{$max: "$score"}}}], function (error, result){
+
+//         if(error) { return console.error(error); }
+//         response.send(result);
+//     });
+// });
+
+
+// app.get('/stat-nb-games', function(request, db) {
+//     db.games.aggregate( [{ "$unwind": "$player" }, { "$group": { "_id": "$player", "nb_games_played": {"$sum": 1 }}} ],  
+//     function (error, result){
+
+//         if(error) { return console.error(error); }
+//         response.send(result);
+//     });
+// });
+
 
 
